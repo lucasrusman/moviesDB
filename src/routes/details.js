7926a3a83b2e4b7aedd20f476798c46105f93ec0
Facundo Rusman <facundo.rusman.04@gmail.com>
2022-03-05 23:12:57 -0300

    ABM details, missing the stored procedure

--- src/routes/details.js
+++ src/routes/details.js
@@ -18,5 +18,73 @@ router.get('/:id', (req, res) => {
   });
 
 
+
+
+  router.get('/:id', (req, res) => {
+    const { id } = req.params;
+    mysqlConnection.query('SELECT * FROM details WHERE id = ?', [id], (err, rows, fields) => {
+      if (!err) {
+        res.json(rows[0]);
+      } else {
+        console.log(err);
+      }
+    });
+  });
+  
+  router.post('/', (req, res) => {
+    const { id,original_title, genrs,  overview, production_companies} = req.body;
+    const query = `
+          CALL detailAdd(?, ?, ?, ?, ?)
+      `;
+    mysqlConnection.query(
+      query,
+      [ id,original_title, genrs,  overview, production_companies],
+      (err, rows, fields) => {
+        if (!err) {
+          res.json({ Status: 'Detail saved' });
+        } else {
+          console.log(err);
+        }
+      }
+    );
+  });
+  
+  router.put('/:id', (req, res) => {
+    const {original_title, genrs,  overview, production_companies } = req.body;
+    const { id } = req.params;
+    const query = `
+          CALL detailEdit(?, ?, ?, ?, ?)
+      `;
+    mysqlConnection.query(
+      query,
+      [id, original_title, genrs,  overview, production_companies],
+      (err, rows, fields) => {
+        if (!err) {
+          res.json({ Status: 'Detail update' });
+        } else {
+          console.log(err);
+        }
+      }
+    );
+  });
+  
+  router.delete('/:id', (req, res) => {
+    const { id } = req.params;
+    mysqlConnection.query('DELETE FROM details WHERE id = ?', [id], (err, rows, fields) => {
+      if (!err) {
+        res.json({ Status: 'Detail delete' });
+      } else {
+        console.log(err);
+      }
+    });
+  });
+
+
+
+
+
+
+
+
   
 module.exports = router;
