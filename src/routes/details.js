1f4e5d91e75aab6614b0b969ad6a504b82606d3e
Facundo Rusman <facundo.rusman.04@gmail.com>
2022-03-05 23:23:08 -0300

    ABM similar, missing the stored procedure

--- src/routes/details.js
+++ src/routes/details.js
@@ -67,12 +67,4 @@ router.delete('/:id', (req, res) => {
   });
 });
 
-
-
-
-
-
-
-
-  
 module.exports = router;
--- src/routes/similars.js
+++ src/routes/similars.js
@@ -18,5 +18,54 @@ router.get('/:id', (req, res) => {
   });
 
 
+
+router.post('/', (req, res) => {
+  const { id, id_similar, original_title, release_date, overview } = req.body;
+  const query = `
+        CALL similarAdd(?, ?, ?, ?, ?)
+    `;
+  mysqlConnection.query(
+    query,
+    [ id, id_similar, original_title, release_date, overview],
+    (err, rows, fields) => {
+      if (!err) {
+        res.json({ Status: 'Similar saved' });
+      } else {
+        console.log(err);
+      }
+    }
+  );
+});
   
+router.put('/:id', (req, res) => {
+  const {id_similar, original_title, release_date, overview  } = req.body;
+  const { id } = req.params;
+  const query = `
+        CALL similarEdit( ?, ?, ?, ?)
+    `;
+  mysqlConnection.query(
+    query,
+    [, id_similar, original_title, release_date, overview ],
+    (err, rows, fields) => {
+      if (!err) {
+        res.json({ Status: 'Similar update' });
+      } else {
+        console.log(err);
+      }
+    }
+  );
+});
+  
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+  mysqlConnection.query('DELETE FROM similars WHERE id = ?', [id], (err, rows, fields) => {
+    if (!err) {
+      res.json({ Status: 'Similar delete' });
+    } else {
+      console.log(err);
+    }
+  });
+});
+
+
 module.exports = router;
