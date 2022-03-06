63a93d8da87aa1427a145cbe15d85751d6a5a411
Facundo Rusman <facundo.rusman.04@gmail.com>
2022-03-05 23:18:48 -0300

    ABM reviews, missing the stored procedure

--- src/routes/reviews.js
+++ src/routes/reviews.js
@@ -15,5 +15,54 @@ router.get('/:id', (req, res) => {
   });
 });
 
+router.post('/', (req, res) => {
+  const {id, author, created_at, content } = req.body;
+  const query = `
+        CALL reviewAdd(?, ?, ?, ?)
+    `;
+  mysqlConnection.query(
+    query,
+    [id, author, created_at, content],
+    (err, rows, fields) => {
+      if (!err) {
+        res.json({ Status: 'Review saved' });
+      } else {
+        console.log(err);
+      }
+    }
+  );
+});
+
+router.put('/:id', (req, res) => {
+  const { author, created_at, content } = req.body;
+  const { id } = req.params;
+  const query = `
+        CALL movieEdit(?, ?, ?)
+    `;
+  mysqlConnection.query(
+    query,
+    [ author, created_at, content],
+    (err, rows, fields) => {
+      if (!err) {
+        res.json({ Status: 'Review update' });
+      } else {
+        console.log(err);
+      }
+    }
+  );
+});
+
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+  mysqlConnection.query('DELETE FROM reviews WHERE id = ?', [id], (err, rows, fields) => {
+    if (!err) {
+      res.json({ Status: 'Review delete' });
+    } else {
+      console.log(err);
+    }
+  });
+});
+
+
 
 module.exports = router;
