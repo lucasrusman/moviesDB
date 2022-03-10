b12025c00f6b2202e9587485f9a3e10c80183fbc
lucasrusman <rusmanlucas@gmail.com>
2022-03-10 16:20:27 -0300

    Completed

--- db/popular.sql
+++ db/popular.sql
@@ -3,9 +3,9 @@ CREATE DATABASE IF NOT EXISTS movieDB;
 USE movieDB;
 
 CREATE TABLE populars (
-	popularity INT (4) NOT NULL,
+	popularity INT (4) DEFAULT NULL,
 	id INT (6) AUTO_INCREMENT,
-    release_date VARCHAR(3000) DEFAULT NULL,
+    realese_date VARCHAR(3000) DEFAULT NULL,
     original_title VARCHAR(3000) DEFAULT NULL,
     overview VARCHAR(3000) DEFAULT NULL,
     PRIMARY KEY (id)
@@ -13,7 +13,7 @@ CREATE TABLE populars (
 
 DESCRIBE populars;
 
-INSERT INTO populars (popularity, release_date, original_title, overview)
+INSERT INTO populars (popularity, realese_date, original_title, overview)
 VALUES (1256, "prueba", "prueba", "prueba");
 
 SELECT * FROM populars ORDER BY popularity DESC;
